1900503e123f0bdffa3f74fb74afdf8e4e3aef40
lu69x <prasit.17.06.1997@gmail.com>
2019-03-16 21:07:28 +0700

    1st deploy

---
+++ app.js
@@ -0,0 +1,28 @@
+var s_port = 41234
+var dgram = require('dgram')
+var server = dgram.createSocket('udp4')
+
+server.on('listening',()=>{
+    var address = server.address()
+    console.log('Server listening ' + address.address + ':' + address.port)
+})
+
+server.on('message',(msg, rinfo)=>{
+    console.log('server got a message from ' + rinfo.address + ':' + rinfo.port);
+    console.log('ASCII: ' + msg);
+    var ack = new Buffer(msg)
+    server.send(ack, 0, ack.length, rinfo.port, rinfo.address, (err,bytes)=>{
+        console.log('sent back')
+    })    
+})
+
+server.on('error',(err)=>{
+    console.log('server error: \n' + err.stack)
+    server.close()
+})
+
+server.on('close',()=>{
+    console.log('Closed.')
+})
+
+server.bind(s_port)
