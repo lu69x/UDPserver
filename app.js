93dd9648c163463d25a2f61fc7f44e93a45a4781
lu69x <prasit.17.06.1997@gmail.com>
2019-03-18 00:41:46 +0700

    test

--- app.js
+++ app.js
@@ -2,18 +2,53 @@ var s_port = 41234
 var dgram = require('dgram')
 var server = dgram.createSocket('udp4')
 
+var n_addr = ''
+var n_port = ''
+var s_addr = ''
+var s_port = ''
+
 server.on('listening',()=>{
     var address = server.address()
     console.log('Server listening ' + address.address + ':' + address.port)
 })
 
 server.on('message',(msg, rinfo)=>{
+    /*
+    Type note
+        Nxx = node to Server (x is number of node) example
+            - N00 : node 0
+            - N09 : node 9
+            - N0A : node 10
+            - NFF : node 255
+        CLI = Client to Server
+        SER = Server to Node
+
     console.log('server got a message from ' + rinfo.address + ':' + rinfo.port);
     console.log('ASCII: ' + msg);
-    var ack = new Buffer(msg)
-    server.send(ack, 0, ack.length, '41234', '119.31.119.95', (err,bytes)=>{
-        console.log('sent back')
-    })    
+    */
+    if(msg[0]=="N"){
+        n_addr = rinfo.address
+        n_port = rinfo.port
+        console.log('Node => Client')
+        /*
+        var ack = new Buffer(msg)
+            server.send(ack, 0, ack.length, rinfo.port, rinfo.address, (err,bytes)=>{
+        }) 
+        */    
+    }else if(msg.slice(0,3)=="CLI"){
+        var ack = new Buffer(msg.slice(3))
+            server.send(ack, 0, ack.length, n_port, n_addr, (err,bytes)=>{
+                console.log('Client => node')
+        })    
+    }else if(msg.slice(0,3)=="SER"){
+        /*
+        var ack = new Buffer(msg)
+            server.send(ack, 0, ack.length, rinfo.port, rinfo.address, (err,bytes)=>{
+            console.log('sent back')
+        })    
+        */
+    }
+    
 })
 
 server.on('error',(err)=>{
@@ -22,7 +57,7 @@ server.on('error',(err)=>{
 })
 
 server.on('close',()=>{
-    console.log('Closed.')
+    console.log('Closed')
 })
 
 server.bind(s_port)
