99e77eac08a6415fd67a2b8dd121f0b8625dcfeb
lu69x <prasit.17.06.1997@gmail.com>
2019-03-18 01:03:24 +0700

    test

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var s_port = 41234
 var dgram = require('dgram')
 var server = dgram.createSocket('udp4')
 
-var n_addr = ''
-var n_port = ''
-var s_addr = ''
-var s_port = ''
+var nodeObj = {
+    addr = '',
+    port = ''
+}
 
 server.on('listening',()=>{
     var address = server.address()
@@ -13,42 +13,21 @@ server.on('listening',()=>{
 })
 
 server.on('message',(msg, rinfo)=>{
-    /*
-    Type note
-        Nxx = node to Server (x is number of node) example
-            - N00 : node 0
-            - N09 : node 9
-            - N0A : node 10
-            - NFF : node 255
-        CLI = Client to Server
-        SER = Server to Node
-
     console.log('server got a message from ' + rinfo.address + ':' + rinfo.port);
     console.log('ASCII: ' + msg);
-    */
-    if(msg[0]=="N"){
-        n_addr = rinfo.address
-        n_port = rinfo.port
-        console.log('Node => Client : ' + msg)
-        /*
+    if(msg.slice(0,1)=='N'){
         var ack = new Buffer(msg)
             server.send(ack, 0, ack.length, rinfo.port, rinfo.address, (err,bytes)=>{
-        }) 
-        */    
-    }else if(msg.slice(0,3)=="CLI"){
-        var ack = new Buffer(msg.slice(3))
-            server.send(ack, 0, ack.length, n_port, n_addr, (err,bytes)=>{
-                console.log('Client => node : ' + msg)
-        })    
-    }else if(msg.slice(0,3)=="SER"){
-        /*
+                nodeObj.addr = rinfo.address
+                nodeObj.port = rinfo.port
+            console.log('Node => Server')
+        })  
+    }else if(msg.slice(0,3)=='CLI'){
         var ack = new Buffer(msg)
-            server.send(ack, 0, ack.length, rinfo.port, rinfo.address, (err,bytes)=>{
-            console.log('sent back')
-        })    
-        */
+            server.send(ack, 0, ack.length, nodeObj.port, nodeObj.addr, (err,bytes)=>{
+            console.log('Client => Node')
+        })  
     }
-    
 })
 
 server.on('error',(err)=>{
@@ -57,7 +36,7 @@ server.on('error',(err)=>{
 })
 
 server.on('close',()=>{
-    console.log('Closed')
+    console.log('Closed.')
 })
 
 server.bind(s_port)
